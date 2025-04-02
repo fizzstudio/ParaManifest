f4b60a2b06cf254b6337fb4fd0cb23b3615cd9cb
simonvarey <simonvarey@gmail.com>
2025-04-02 17:43:13 -0400

    Schema string enums now generate string literal union TS types

--- build/build_types.ts
+++ build/build_types.ts
@@ -2,8 +2,10 @@ import { appendFileSync } from 'fs';
 import { compileFromFile } from 'json-schema-to-typescript';
 
 compileFromFile(
-  `./schema/manifest.schema.json`, 
-  { ignoreMinAndMaxItems: true }
+  `./schema/manifest.schema.json`, { 
+    ignoreMinAndMaxItems: true,
+    inferStringEnumKeysFromValues: true
+  }
 ).then(
   ts => appendFileSync(`./dist/paramanifest.d.ts`, ts)
 );
