b3cab09a2531f2abfefb9ff330fd24ac23e5253b
simonvarey <simonvarey@gmail.com>
2025-06-27 14:10:09 -0400

    Fixed schema documentation

--- build/build_docs.ts
+++ build/build_docs.ts
@@ -2,11 +2,12 @@ import JsonSchemaStaticDocs from 'json-schema-static-docs';
 
 (async () => {
   const jsonSchemaStaticDocs = new JsonSchemaStaticDocs({
-    inputPath: '../schema',
-    outputPath: 'docs',
+    inputPath: 'schema',
+    outputPath: 'docs/manifest',
     jsonSchemaVersion: 'https://json-schema.org/draft/2020-12/schema',
     ajvOptions: {
       allowUnionTypes: true,
+      strictSchema: 'log'
     },
   });
   await jsonSchemaStaticDocs.generate();
