7a1ba9f5cb26b46ce83c19cc3eb3494b89ede243
simonvarey <simonvarey@gmail.com>
2025-11-12 12:50:20 -0500

    strtoId now collapses all non-alphanum

--- test/normalizeid.test.ts
+++ test/normalizeid.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from 'vitest';
 import { strToId } from '../lib';
 
 test('strToId', () => {
-  expect(strToId('  aB$)\t  Cd$$ ')).toBe('_ab___cd___');
+  expect(strToId('  aB$)\t  Cd$$ ')).toBe('_ab_cd_');
 })
